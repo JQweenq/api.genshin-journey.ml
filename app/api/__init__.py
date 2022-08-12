3eadf0b896aef6b974e4f6a635bfbecb2b6e4364
JQweenq <josty.qweenq@gmail.com>
2022-08-13 02:25:36 +0700

    update auth architecture

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,8 +1,7 @@
 from flask import Blueprint
-from flask_restful import Api
 
-from app.api.auth import RegistrationRoute, LoginRoute, LogoutRoute
 from app.api import (
+    auth,
     characters,
     dictionary,
     meta,
@@ -13,14 +12,9 @@ from app.api import (
 
 api: Blueprint = Blueprint('api', __name__)
 
+api.register_blueprint(auth.route)
 api.register_blueprint(characters.route)
 api.register_blueprint(dictionary.route)
 api.register_blueprint(meta.route)
 api.register_blueprint(weapons.route)
 api.register_blueprint(wishes.route)
-
-rest: Api = Api(api)
-
-rest.add_resource(RegistrationRoute, '/register')
-rest.add_resource(LoginRoute, '/login')
-rest.add_resource(LogoutRoute, '/logout')
