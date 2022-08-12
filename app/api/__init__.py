d3b02eb3438dce039e2c532ed4c13448d7bb254e
JQweenq <josty.qweenq@gmail.com>
2022-08-13 00:18:04 +0700

    update character architecture

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,7 +2,9 @@ from flask import Blueprint
 from flask_restful import Api
 
 from app.api.auth import RegistrationRoute, LoginRoute, LogoutRoute
-from app.api.characters import CharactersRoute
+from app.api import (
+    characters
+)
 from app.api.dictionary import DictionaryRoute
 from app.api.meta import MetaRoute
 from app.api.weapons import WeaponsRoute
@@ -10,12 +12,13 @@ from app.api.wishes import WishesRoute
 
 api: Blueprint = Blueprint('api', __name__)
 
+api.register_blueprint(characters.route)
+
 rest: Api = Api(api)
 
 rest.add_resource(RegistrationRoute, '/register')
 rest.add_resource(LoginRoute, '/login')
 rest.add_resource(LogoutRoute, '/logout')
-rest.add_resource(CharactersRoute, '/characters')
 rest.add_resource(DictionaryRoute, '/dictionary')
 rest.add_resource(WishesRoute, '/wishes')
 rest.add_resource(WeaponsRoute, '/weapons')
--- app/api/characters.py
+++ app/api/characters.py
@@ -1,33 +1,43 @@
-from flask import request
-from flask_restful import Resource
+from flask import Blueprint, request
 
 from app.api.base import BaseResource
+from app.data_models.character import CharacterData
 from app.models.character import Character
-from app.utils.datas import *
+from app.utils.datas import (
+    GET,
+    POST,
+    DELETE,
+    PATCH
+)
+
+__all__ = ["route"]
+
+route = Blueprint("characters", __name__, url_prefix="characters")
 
 
-class CharactersRoute(Resource):
+@route.get("/")
+def get():
+    args: GET = request.parse()
 
-    @staticmethod
-    def get() -> (dict, int):
-        args: GET = request.parse()
+    return BaseResource.get(Character, args)
 
-        return BaseResource.get(Character, args)
 
-    @staticmethod
-    def post() -> (dict, int):
-        args: POST = request.parse(['name', 'name_en'])
+@route.post("/")
+def post():
+    args: POST = request.parse(["name", "name_en"])
 
-        return BaseResource.post(Character, args)
+    return BaseResource.post(Character, args)
 
-    @staticmethod
-    def delete():
-        args: DELETE = request.parse(['id'])
 
-        return BaseResource.delete(Character, args)
+@route.delete("/")
+def delete():
+    args: DELETE = request.parse(["id"])
 
-    @staticmethod
-    def patch():
-        args: PATCH = request.parse(['id', 'attr', 'value'])
+    return BaseResource.delete(Character, args)
 
-        return BaseResource.patch(Character, CharacterData, args)
+
+@route.patch("/")
+def patch():
+    args: PATCH = request.parse(["id", "attr", "value"])
+
+    return BaseResource.patch(Character, CharacterData, args)
