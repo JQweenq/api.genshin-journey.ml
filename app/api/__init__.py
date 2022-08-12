151d9d2991fff6e147ce41a3f622508dc6e33e37
JQweenq <josty.qweenq@gmail.com>
2022-08-13 00:44:01 +0700

    update dictionary architecture

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,9 +3,9 @@ from flask_restful import Api
 
 from app.api.auth import RegistrationRoute, LoginRoute, LogoutRoute
 from app.api import (
-    characters
+    characters,
+    dictionary
 )
-from app.api.dictionary import DictionaryRoute
 from app.api.meta import MetaRoute
 from app.api.weapons import WeaponsRoute
 from app.api.wishes import WishesRoute
@@ -13,13 +13,13 @@ from app.api.wishes import WishesRoute
 api: Blueprint = Blueprint('api', __name__)
 
 api.register_blueprint(characters.route)
+api.register_blueprint(dictionary.route)
 
 rest: Api = Api(api)
 
 rest.add_resource(RegistrationRoute, '/register')
 rest.add_resource(LoginRoute, '/login')
 rest.add_resource(LogoutRoute, '/logout')
-rest.add_resource(DictionaryRoute, '/dictionary')
 rest.add_resource(WishesRoute, '/wishes')
 rest.add_resource(WeaponsRoute, '/weapons')
 rest.add_resource(MetaRoute, '/meta')
